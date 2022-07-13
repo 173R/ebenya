a24e24bf6d7604b462201283deee8245b1838894
Artem Getman <artyom.getman1996@gmail.com>
2022-07-14 00:07:06 +1000

    add: render passes

--- src/lib.rs
+++ src/lib.rs
@@ -20,20 +20,23 @@ impl State {
     async fn new(window: &Window) -> Self {
         let size = window.inner_size();
 
+        //Инстанс самого wgpu
         let instance = wgpu::Instance::new(wgpu::Backends::all());
+        //Поверхность для отображения
         let surface = unsafe { instance.create_surface(window)};
-        let adapter = instance.request_adapter(
+        //Хэндлер видекарты
+        let adapter = instance.request_adapter( 
             &wgpu::RequestAdapterOptions {
                 power_preference: wgpu::PowerPreference::default(),
                 compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
+                force_fallback_adapter: false, // true - максимальная поддержка всех платформ, софтварный рендер
             },
         ).await.unwrap();
 
         let (device, queue) = adapter.request_device(
             &wgpu::DeviceDescriptor {
                 features: wgpu::Features::empty(),
-                limits: if cfg!(target_arch = "wasm32") {
+                limits: if cfg!(target_arch = "wasm32") { // задаём лимиты для максимальной поддержки всех бэкендов
                     wgpu::Limits::downlevel_webgl2_defaults()
                 } else {
                     wgpu::Limits::default()
@@ -43,11 +46,14 @@ impl State {
             None,
         ).await.unwrap();
 
+
+        //Конфигурируем surface
         let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface.get_preferred_format(&adapter).unwrap(),
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT, //Текстуры будут юзаться для вывода на экран
+            format: surface.get_supported_formats(&adapter)[0], // как будут хранится SurfaceTexture в GPU
             width: size.width,
             height: size.height,
+            //Формат синхронизации поверхности с дисплеем, Fifo ограничивает частоту кадров, поодерживается везде, по сути это VSync
             present_mode: wgpu::PresentMode::Fifo,
         };
 
@@ -63,19 +69,61 @@ impl State {
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!()
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.width = new_size.width;
+            self.surface.configure(&self.device, &self.config);
+        }
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        todo!()
+        false
     }
 
     fn update(&mut self) {
-        todo!()
+        println!("aaaa");
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!()
+        //Достаём текстуру из поверхности
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(
+            &wgpu::TextureViewDescriptor::default()
+        );
+        //Кодировщик нужен для создания буфера команд которые потом пойдут в GPU
+        let mut encoder = self.device.create_command_encoder(
+            &wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            }
+        );
+
+        //Создаём проход рендера
+        
+        {//так как созданный _render_pass владеет encoder 
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 1.0,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+        //Завершить буфер команд и отправить его в очередь
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 
 }
@@ -117,19 +165,48 @@ pub async fn run() {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
+        } if window_id == window.id() => if !state.input(event) {
+            match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                },
+                WindowEvent::ScaleFactorChanged {
+                    new_inner_size, ..
+                } => state.resize(**new_inner_size),
+                _ => {}
+            }
         },
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        },
+        Event::MainEventsCleared => {
+            //Получается этот ивент тригерится первый раз при создании окна??
+            //И потом запрашивает перерисовку
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            //println!("aaaa");
+            //Явно запрашиваем перерисовку
+            window.request_redraw();
+        }
         _ => {}
     });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ebenya::run;
 
 fn main() {
-    run();
+    pollster::block_on(run());
 }
             
