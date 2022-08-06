55e8c2989443edad28bb921afc5b3528c13e5cb5
173R <artyom.getman1996@gmail.com>
2022-08-06 18:03:30 +1000

    redesign: matrix

--- src/vmath/matrix.rs
+++ src/vmath/matrix.rs
@@ -3,76 +3,77 @@ use std::{fmt::Debug, ops::Mul};
 use num::Float;
 
 #[derive(Copy, Clone)]
-pub struct Matrix<T: Float, const R: usize, const C: usize>(pub [[T; C]; R]);
+pub struct SquareMatrix<T: Float, const S: usize> {
+    pub data: [[T; S]; S],
+}
 
-impl<T: Float + Default + Debug, const R: usize, const C: usize> Matrix<T, R, C> {
+impl<T: Float + Default + Debug, const S: usize> SquareMatrix<T, S> {
     pub fn new() -> Self {
-        Self ([[Default::default(); C]; R])
+        Self {
+            data: [[Default::default(); S]; S] 
+        }
     }
 
-    pub fn new_from_array(array: [[T; C]; R]) -> Self {
-        Self (array)
+    pub fn new_from_array(array: [[T; S]; S]) -> Self {
+        Self {
+            data: array 
+        }
     }
 
-    pub fn new_translation(translation: &[T; R]) -> Self {
+    pub fn new_translation(translation: &[T; S]) -> Self {
         let mut matrix = Self::new_indentity();
-        for row in 0..R {
-            matrix.0[row][C - 1] = translation[row];
+        for row in 0..S {
+            matrix.data[row][S - 1] = translation[row];
         }
 
         matrix
     }
 
     pub fn new_indentity() -> Self {
-        Self::new_scale(&[T::one(); R])
+        Self::new_scale(&[T::one(); S])
     } 
 
-    pub fn new_scale(scale: &[T; R]) -> Self {
+    pub fn new_scale(scale: &[T; S]) -> Self {
         let mut scale_matrix = Self::new();
-        for row in 0..scale_matrix.0.len() {
-            for col in 0..scale_matrix.0[row].len() {
+        for row in 0..scale_matrix.data.len() {
+            for col in 0..scale_matrix.data[row].len() {
                 if row == col {
-                    scale_matrix.0[row][col] = scale[row];
+                    scale_matrix.data[row][col] = scale[row];
                 }
             }
         }
         scale_matrix
     }
 
-    // fn set_diagonal(&self, diagonal: &[T; R]) -> Self {
-    //     let mut matrix = Self::new();
-    //     for row in 0..matrix.0.len() {
-    //         for col in 0..matrix.0[row].len() {
-    //             if row == col {
-    //                 matrix.0[row][col] = diagonal[row];
-    //             }
-    //         }
+    // pub fn transpose(&mut self) {
+    //     let mut matrix = *self;
+    //     for (index, row) in matrix.0.iter().enumerate() {
+    //        for value in row {
+    //         self.0[index][] = value;
+    //        } 
     //     }
-
-    //     matrix
     // }
+
 }
 
 
 impl<T: Float> From<Matrix4x4<T>> for [[T; 4]; 4] {
     fn from(matrix: Matrix4x4<T>) -> Self {
-        matrix.0
+        matrix.data
     }
 }
 
-//Проблема с кубическими и не кубическими матрицами
-//Только для кубической матрицы
-impl<T, const S: usize> Mul<Matrix<T, S, S>> for Matrix<T, S, S>
+impl<T, const S: usize> Mul<SquareMatrix<T, S>> for SquareMatrix<T, S>
 where
     T: Float + Default + Debug + std::ops::AddAssign,
 {
-    type Output = Matrix<T, S, S>;
-    fn mul(self, rhs: Matrix<T, S, S>) -> Matrix<T, S, S> {
+    type Output = SquareMatrix<T, S>;
+    fn mul(self, rhs: SquareMatrix<T, S>) -> SquareMatrix<T, S> {
         let mut result = Self::new();
         for i in 0..S {
             for j in 0..S {
                 for k in 0..S  {
-                    result.0[j][i] += self.0[i][k] * rhs.0[k][i];   
+                    result.data[j][i] += self.data[i][k] * rhs.data[k][i];   
                 }
             }
         }
@@ -81,10 +82,10 @@ where
     }
 }
 
-impl<T: Float + Default + Debug, const R: usize, const C: usize> Debug for Matrix<T, R, C> {
+impl<T: Float + Debug, const S: usize> Debug for SquareMatrix<T, S> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0[..].fmt(f)
+        self.data[..].fmt(f)
     }
 }
 
-pub type Matrix4x4<T> = Matrix<T, 4, 4>;
+pub type Matrix4x4<T> = SquareMatrix<T, 4>;
